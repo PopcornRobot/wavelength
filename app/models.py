338b260115169b4d62227bae46f31a906851389f
Kelvin Tsoi <tsoikelvin@gmail.com>
2022-08-09 21:46:52 -0700

    refactor(wavelength): clean up string interpolation in models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Question(models.Model):
     # user = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
     question = models.CharField(max_length=50)
@@ -15,10 +13,9 @@ class GameRoom(models.Model):
     is_game_active = models.BooleanField(default=False)
     players = models.IntegerField(default=0)
     teams = models.IntegerField(default=0)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.game_id) + " " + self.room_key + " " + str(self.is_game_active) + " " + str(self.players) + " " + str(self.created_at) + " " + str(self.updated_at)
+        return f"{str(self.game_id)} {self.room_key} {self.is_game_active} {self.players)} {self.created_at)} {self.updated_at)}"
 
