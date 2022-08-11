88edd840a67f37b7be8da2768ed6532faee21015
ArleneRangel <arlenemrangel@gmail.com>
2022-08-11 18:30:41 -0500

    feat(added the new model for gameturn): created GameTurn model

--- app/models.py
+++ app/models.py
@@ -19,3 +19,18 @@ class GameRoom(models.Model):
     def __str__(self):
         return f"{str(self.game_id)} {self.room_key} {self.is_game_active} {self.players} {self.created_at} {self.updated_at}"
 
+class GameTurn(models.Model):
+    team_id = models.ForeignKey("Team", on_delete=models.CASCADE)
+    session_id = models.ForeignKey("Game", on_delete=models.CASCADE)
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
+    clue_giver = models.ForeignKey("User", on_delete=models.CASCADE)
+    clue_given = models.CharField(max_length=100)
+    game_answer = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    team_answer = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.team_id} {self.session_id} {self.question} {self.clue_giver} {self.clue_given} {self.game_answer} {self.created_at} {self.team_answer}"
+
+            
+
