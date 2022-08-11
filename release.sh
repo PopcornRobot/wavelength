ddd14ef0ca442d92464bc7b55b92e0f1e50f138b
Kelvin Tsoi <tsoikelvin@gmail.com>
2022-08-10 22:43:29 -0700

    fix(wavelength): remove release.sh and run in heroku.yml

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# collect static on release
-python manage.py collectstatic --noinput -i admin -i django_extensions -i debug_toolbar
-
-# run migrate on release
-python manage.py migrate
