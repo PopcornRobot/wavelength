476cacc73d74df8be4ba764932c2f5a5c4f74cc3
AndresRoc <andres.rodriguez.campo@gmail.com>
2022-08-15 22:40:21 -0700

    fix(wavelength): models redesign and organization
    The models have been re-designed and several objects had changed their names and properties

--- app/models.py
+++ app/models.py
@@ -1,44 +1,55 @@
+from statistics import mode
 from django.db import models
 
-# This model stores the left & right spectrums indicators 
-class Question(models.Model):
-    left_spectrum = models.CharField(max_length=50)
-    right_spectrum = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.left_spectrum} {self.right_spectrum} {self.created_at} {self.updated_at}"
 
 # This models is only to enable the Game, the game's id will be used as the session ID required for the teams & game turn creation
 class Game(models.Model):
-    is_game_waiting = models.BooleanField(default=False)
+    is_game_waiting = models.BooleanField(default=True)
     is_game_running = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.is_game_waiting} {self.is_game_running} {self.created_at} {self.updated_at}"
-    
-class Player(models.Model):
-    username = models.CharField(max_length = 20)
+        return f" is the game open: {self.is_game_waiting}, The game status is: {self.is_game_running}, created: {self.created_at}, updated: {self.updated_at}"
 
-    def __str__(self):
-        return self.player
 
 # This models will store the information of the players , team name and scores
 class Team(models.Model):
-    team_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     score = models.IntegerField(default=0)
-    game_session = models.ForeignKey(Game, on_delete=models.CASCADE) #connect to Game model
-    players = models.ForeignKey(Player, on_delete=models.CASCADE) #connect to Player model
+    game = models.ForeignKey(Game, on_delete=models.CASCADE) #connect to Game model
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.team_name} {self.score} {self.session} {self.players} {self.created_at} {self.updated_at}"
+        return f" the team name is: {self.name}, the team's score is:{self.score}, the current game is: {self.game}, created: {self.created_at}, updated: {self.updated_at}"
 
-# TODO: will uncomment when models are finalize, temporary commented out
+# This model stores the player's information. The information consists on the team and the game that the player is either staring or joining
+# The team object is populated once the host player "Starts the Game"
+# Te game object is populated once the player selects the join/start a new game.
+class Player(models.Model):
+    username = models.CharField(max_length = 20)
+    is_host = models.BooleanField(default=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=True, null=True)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f" the current player is: {self.username}, is this the host: {self.is_host}, the players team is: {self.team}, the player's game is: {self.game}, created: {self.created_at}, updated: {self.updated_at}"
+
+
+# This model stores the left & right spectrums indicators 
+class Question(models.Model):
+    left_spectrum = models.CharField(max_length=100)
+    right_spectrum = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f" the left spectrum is: {self.left_spectrum}, the right spectrum is: {self.right_spectrum}, created: {self.created_at}, updated: {self.updated_at}"
+
+# This will store the questions previously assigned
 class QuestionHistory(models.Model):
     player = models.OneToOneField(Player, on_delete=models.CASCADE) #connect to Player model
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #connect to Question model
@@ -46,21 +57,22 @@ class QuestionHistory(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.player} {self.question}"
+        return f" the player assigned with the question is: {self.player}, used question: {self.question}, created: {self.created_at}, updated: {self.updated_at}"
 
+# This model stores the game history (responses, questions, teams and clues)
 class GameTurn(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    game_session = models.ForeignKey(Game, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    clue_giver = models.ForeignKey(Player, on_delete=models.CASCADE)
-    clue_given = models.CharField(max_length=100)
-    game_answer = models.IntegerField(default=0)
+    clue_giver = models.ForeignKey(Player, on_delete=models.CASCADE) # Clue is the response that the polayer provides 
+    clue_given = models.CharField(max_length=100) 
+    question_answer = models.IntegerField(default=0) # Question answer is randomly assigned
+    team_answer = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    team_answer = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.team} {self.session} {self.question} {self.clue_giver} {self.clue_given} {self.game_answer} {self.created_at} {self.updated_at} {self.team_answer}"
+        return f" the team in the room is:{self.team}, the game is: {self.game}, the question is:{self.question}, the clue giver is: {self.clue_giver}, the previous clue is: {self.clue_given}, the current question answer is: {self.question_answer}, the team answer is {self.team_answer}, created: {self.created_at}, updated: {self.updated_at}"
 
             
 
