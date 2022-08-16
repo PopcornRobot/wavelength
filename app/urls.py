a34ee43d96edf77b1047c453e6e36d7003ba1748
JINDOUJIANG <jindoujiang@outlook.com>
2022-08-15 22:57:19 -0700

    feat(wavelength): new start page

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("start_page", views.start_page, name='start_page'),
+    path("player_registration_form", views.player_registration_form, name='player_registration_form'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,39 @@
-from django.http import HttpResponse
+from unicodedata import name
+from unittest import expectedFailure
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from django.shortcuts import render
+from django.urls import reverse
+from .models import *
+import random
+
+teams_placeholder = {}
 
 def index(request):
     return HttpResponse("Hello, world")
+
+
+
+# This function opens a file that stores a list of 250 team names and resturns a random name from the list
+def random_name():
+    # open the file
+    with open("app/static/txt/team_names.csv") as f:
+        # Assigns all the lines
+        name_list = f.readlines()
+        # Random assignation of a single name from the list
+        new_name = name_list[random.randint(0,len(name_list))]
+    # Returns a new randome name
+    return new_name
+
+# When a player wants to play he/she has to type a username and select start/join.
+# creating the player objects
+# When a player wants to play the game selects "Start Game" or "Join game"
+# the player is redirected to this function where the game objects are created
+def start_page(request):
+    context = {}
+    return render(request, "wavelength/start_page.html", context)
+
+def player_registration_form(request):
+    if request.method == 'POST':
+        # Assigns the form input to the players name
+        player_name = request.POST['name']
+    return HttpResponseRedirect(reverse('app:start_page'))
