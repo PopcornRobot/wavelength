b956cdf6f4364d7bd44433f5f759f69116fc5b67
JINDOUJIANG <jindoujiang@outlook.com>
2022-08-17 21:32:45 -0700

    fix(wavelength): add app_name into url

--- wavelength/urls.py
+++ wavelength/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+app_name="app"
+
 urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
