09167b8f74d03cb6d0cc964e4b90450b92314360
JINDOUJIANG <jindoujiang@outlook.com>
2022-08-18 19:27:12 -0700

    fix(wavelength): get rid of namespace on project url

--- wavelength/urls.py
+++ wavelength/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-app_name="app"
-
 urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
