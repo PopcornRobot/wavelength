123cd6bc028d5f74cc60cb6b18e07c416fd08f76
Kelvin Tsoi <tsoikelvin@gmail.com>
2022-08-22 19:22:18 -0700

    fix(wavelength): explicit url pattern for checking content rendering

--- app/tests/test_samples.py
+++ app/tests/test_samples.py
@@ -13,7 +13,7 @@ def test_web_index_view(client):
     """
     A sample assertion test for index web view
     """
-    url = reverse("index")                          # note this is the url pattern name
+    url = "/"                                       # note this is the url pattern name
     response = client.get(url)                      # pass client object to render page with get request
     assert response.status_code == 200              # testing 200 response, we can test 404s etc etc
     assert response.content == b"Hello, world"      # check body content for text string
