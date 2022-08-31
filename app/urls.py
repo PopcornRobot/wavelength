e7d1224a37e2e90b5d403bb83bb46d93be34f16c
AndresRoc <andres.rodriguez.campo@gmail.com>
2022-08-30 22:22:51 -0700

    base page completion

--- app/urls.py
+++ app/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     path('team_creation/<int:player_game>/<int:player_id>', views.team_creation, name='team_creation'),
     # AC
     path('team_page/<int:host_id>/<int:game_id>', views.team_page, name='team_page'),
+    path('player_game_assignation/<int:game_id>', views.player_game_assignation, name='player_game_assignation'),
     path('player_game_assignation/<int:game_id>/<int:player_id>', views.player_game_assignation, name='player_game_assignation'),
     
+    path('game_list', views.game_list, name='game_list'),
     path('game_list/<int:player_id>', views.game_list, name='game_list'),
+    
     path('game_session/<int:game_id>', views.game_session, name='game_session'),
     path("start_page", views.start_page, name='start_page'),
     path("host_player_registration_form", views.host_player_registration_form, name='host_player_registration_form'),
