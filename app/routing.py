f2ac5aee6b7c340b4bf4107f94bde315f0a3e1bc
JomanDiec <jobiochem@yahoo.com>
2022-09-15 22:59:47 -0700

    feat(wavelength): adding websockets to question clue page
    using websockets to confirm clues have been submitted by all players before moving to game turn page

--- app/routing.py
+++ app/routing.py
@@ -5,4 +5,5 @@ from . import consumers
 websocket_urlpatterns = [
   path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()), # Using asgi
   path('ws/game_session/<int:room_name>', consumers.ChatConsumer.as_asgi()),
+  path('ws/team_session/<int:room_name>', consumers.ChatConsumer.as_asgi()),
 ]
