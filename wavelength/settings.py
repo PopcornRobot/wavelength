b9fce3c06fd0cf1d9a2a57ce9f5f36da8ba84847
Brandon Madison <brandmadis@gmail.com>
2022-10-04 16:24:07 -0700

    fix(wavelength): add whitenoise middleware to settings

--- wavelength/settings.py
+++ wavelength/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wavelength.urls'
